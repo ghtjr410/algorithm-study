c7a24b41c83c9029b1d2bbec6b952de815c5ea47
ghtjr410 <ghtjr410@gmail.com>
2024-08-25 08:15:28 +0900

    문제 번호 1000 문제 내용 : A+B

--- src/A_0825/Main0002.java
+++ src/A_0825/Main0002.java
@@ -1,7 +1,14 @@
 package A_0825;
 
+import java.util.Scanner;
+
 public class Main0002 {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        System.out.println(A+B);
     }
 }
