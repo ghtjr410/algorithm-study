f0cf731840e21655039e41b9b63af67b7b340ec8
ghtjr410 <ghtjr410@gmail.com>
2024-08-25 07:51:10 +0900

    1. algorithm 초기 프로젝트 생성 2. devcontainer 안에서 알고리즘 테스트 3. hello world 문제 성공

---
+++ src/A_0825/Main0001.java
@@ -0,0 +1,7 @@
+package A_0825;
+
+public class Main0001 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/A_0825/Main0002.java
@@ -0,0 +1,7 @@
+package A_0825;
+
+public class Main0002 {
+    public static void main(String[] args) {
+        
+    }
+}
