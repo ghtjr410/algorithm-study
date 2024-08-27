ca18c639962da3ac2d99fb4b49dc0940ff995c33
ghtjr410 <ghtjr410@gmail.com>
2024-08-27 22:28:41 +0900

    문제번호 : 백준18108번 문제내용 : ICPC Bangkok Regional에 참가하기 위해 수완나품 국제공항에 막 도착한 팀 레드시프트 일행은 눈을 믿을 수 없었다. 공항의 대형 스크린에 올해가 2562년이라고 적혀 있던 것이었다.
    불교 국가인 태국은 불멸기원(佛滅紀元), 즉 석가모니가 열반한 해를 기준으로 연도를 세는 불기를 사용한다. 반면, 우리나라는 서기 연도를 사용하고 있다. 불기 연도가 주어질 때 이를 서기 연도로 바꿔 주는 프로그램을 작성하시오.

---
+++ src/A_0827/Main_0008.java
@@ -0,0 +1,16 @@
+package A_0827;
+
+import java.util.Scanner;
+
+public class Main_0008 {
+    public static void main(String[] args) {
+    
+        Scanner sc = new Scanner(System.in);
+        int bYear = sc.nextInt();
+
+        int sYear = bYear-543;
+        System.out.println(sYear);
+
+        sc.close();
+    }
+}
