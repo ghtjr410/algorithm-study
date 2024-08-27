9a8bb18c14cad64e4eaa1d226a05629be25f11b7
ghtjr410 <ghtjr410@gmail.com>
2024-08-27 23:17:38 +0900

    문제번호 : 백준10171번 문제내용 : \    /\  )  ( ') (  /  )  \(__)| 고양이 출력문제

---
+++ src/A_0827/Main_0012.java
@@ -0,0 +1,10 @@
+package A_0827;
+
+public class Main_0012 {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
