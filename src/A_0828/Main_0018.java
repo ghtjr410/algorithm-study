f67e25d5b614293c2311ef292691d8c20d641ae4
ghtjr410 <ghtjr410@gmail.com>
2024-08-28 11:03:02 +0900

    문제번호 : 백준2884번 문제내용 : 상근이는 매일 아침 알람을 듣고 일어난다. 알람을 듣고 바로 일어나면 다행이겠지만, 항상 조금만 더 자려는 마음 때문에 매일 학교를 지각하고 있다.
    상근이는 모든 방법을 동원해보았지만, 조금만 더 자려는 마음은 그 어떤 것도 없앨 수가 없었다.
    
    이런 상근이를 불쌍하게 보던 창영이는 자신이 사용하는 방법을 추천해 주었다.
    
    바로 "45분 일찍 알람 설정하기"이다.
    
    이 방법은 단순하다. 원래 설정되어 있는 알람을 45분 앞서는 시간으로 바꾸는 것이다. 어차피 알람 소리를 들으면, 알람을 끄고 조금 더 잘 것이기 때문이다. 이 방법을 사용하면, 매일 아침 더 잤다는 기분을 느낄 수 있고, 학교도 지각하지 않게 된다.
    
    현재 상근이가 설정한 알람 시각이 주어졌을 때, 창영이의 방법을 사용한다면, 이를 언제로 고쳐야 하는지 구하는 프로그램을 작성하시오.
    
    첫째 줄에 두 정수 H와 M이 주어진다. (0 ≤ H ≤ 23, 0 ≤ M ≤ 59) 그리고 이것은 현재 상근이가 설정한 알람 시간 H시 M분을 의미한다.
    
    입력 시간은 24시간 표현을 사용한다. 24시간 표현에서 하루의 시작은 0:0(자정)이고, 끝은 23:59(다음날 자정 1분 전)이다. 시간을 나타낼 때, 불필요한 0은 사용하지 않는다
    
    첫째 줄에 상근이가 창영이의 방법을 사용할 때, 설정해야 하는 알람 시간을 출력한다. (입력과 같은 형태로 출력하면 된다.)

---
+++ src/A_0828/Main_0018.java
@@ -0,0 +1,73 @@
+package A_0828;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main_0018 {
+    private static final int ZERO = 0;
+    private static final int HOUR_UPPER_BOUND = 24;
+    private static final int MINUTE_UPPER_BOUND = 60;
+    private static final int MINUS_MINUTE = 45;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int hour = getVaildHour(sc);
+        int minute = getVaildMinute(sc);
+
+        int[] result = calculateResult(hour, minute);
+        System.out.println(result[0] + " " + result[1]);
+    }
+
+    private static int getVaildHour(Scanner sc) { 
+        int hour;
+        
+        while (true) {
+            try {
+                hour = sc.nextInt();
+                if (isVaildHour(hour)) break;
+                
+            } catch (InputMismatchException e) {
+                System.out.println("Error : Please Enter a Valid Integer");
+            }            
+        }
+        return hour;
+    }
+    private static int getVaildMinute(Scanner sc) { 
+        int minute;
+        while (true) {
+            try {
+                minute = sc.nextInt();
+                if (isVaildMinute(minute)) break;
+            } catch (InputMismatchException e) {
+                System.out.println("Error : Please Enter a Valid Integer");
+                sc.next();
+            }
+        }
+        return minute;
+    }
+
+    private static boolean isVaildHour (int hour) {
+        return hour >= ZERO && hour < HOUR_UPPER_BOUND;
+    }
+
+    private static boolean isVaildMinute (int minute) {
+        return minute >= ZERO && minute < MINUTE_UPPER_BOUND;
+    }
+
+    private static int[] calculateResult(int hour, int minute) {
+        int resHour = hour;
+        int resMinute = minute;
+        
+        if (minute < MINUS_MINUTE) {
+            resMinute = MINUTE_UPPER_BOUND - (MINUS_MINUTE - minute);
+            resHour -= 1;
+            if (resHour < 0) {
+                resHour = HOUR_UPPER_BOUND - 1;  // 23시로 조정
+            }
+        } else {
+            resMinute -= MINUS_MINUTE;
+        }
+
+        return new int[]{resHour, resMinute};
+    }
+}
