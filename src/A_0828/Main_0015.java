6fdb4935d8f0616a55093a7f261ce578240ea895
ghtjr410 <ghtjr410@gmail.com>
2024-08-28 09:28:21 +0900

    문제번호 : 백준9498번 문제내용 : 시험 점수를 입력받아 90 ~ 100점은 A, 80 ~ 89점은 B, 70 ~ 79점은 C, 60 ~ 69점은 D, 나머지 점수는 F를 출력하는 프로그램을 작성하시오. 첫째 줄에 시험 점수가 주어진다. 시험 점수는 0보다 크거나 같고, 100보다 작거나 같은 정수이다.

---
+++ src/A_0828/Main_0015.java
@@ -0,0 +1,28 @@
+package A_0828;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main_0015 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int score = -1;
+
+        while (true) {
+            try {
+                score = sc.nextInt();
+                if(score >=0 && score <= 100) break;
+                System.out.println("0이상 100이하의 정수를 입력하세요");
+            } catch (InputMismatchException e) {
+                System.out.println("Error : Please enter a valid integer");
+                sc.next();
+            }
+        }
+        
+        if ( score >= 90) System.out.println("A");
+        else if ( score >= 80 ) System.out.println("B");
+        else if ( score >= 70 ) System.out.println("C");
+        else if ( score >= 60 ) System.out.println("D");
+        else System.out.println("F");
+    }
+}
